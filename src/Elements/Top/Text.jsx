ef3c101c12cfd675f21ca043ce0250e61b0aa595
Danylo Sus <danil.sushko19@gmail.com>
2023-07-30 18:44:01 +0300

    Animations

--- src/Elements/Top/Text.jsx
+++ src/Elements/Top/Text.jsx
@@ -5,7 +5,7 @@ const Text = () => {
     <div className="text-gray font-light text-[54px] md:text-2xl">
       <h1>
         We are launching{" "}
-        <span className="font-bold text-very-dark-blue">soon!</span>
+        <span className="font-bold text-very-dark-blue anim-soon">soon!</span>
       </h1>
       <p className="text-xl md:text-xs mt-5 font-semibold">
         Subscribe and get notified
